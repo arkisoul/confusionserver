ca636384e13cdede9d6251a235db3423ace268ac
Arpit <arpit_jain@live.com>
2018-03-05 22:56:51 +0530

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -3,6 +3,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // Used to create, sign, and verify token
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var User = require('./models/user');
 var config = require('./config');
@@ -36,12 +37,38 @@ exports.verifyUser = passport.authenticate('jwt', {session: false});
 
 exports.verfiyAdmin = (req, rest, next) => {
     if (req.user.admin) {
-        console.log('admin');
         next();
     } else {
-        console.log('not an admin');
         var err = new Error('You are not authorized to perform this operation');
         err.status = 403;
         next(err);
     }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            } else {
+                let user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    else {
+                        return done(null, user);
+                    }
+                });
+            }
+        });
+    }
+));
